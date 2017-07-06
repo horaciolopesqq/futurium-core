9191a91c68fd25be06e97118f3a803eb449b20f5
Horacio Lopes <HoracioLopes@gmail.com>
2017-07-06 10:36:16 +0200

    Improve futurium linker comments

---
+++ modules/custom/futurium_linker/README.txt
@@ -0,0 +1,16 @@
+Futurium Linker
+===============
+
+This module allows establishing relations between nodes with a simple textfield.
+
+
+Installation
+============
+
+Install the module the normal Drupal way.
+
+
+Important Note:
+===============
+
+This module only works to create relations between node entities.
--- modules/custom/futurium_linker/futurium_linker.api.php
+++ modules/custom/futurium_linker/futurium_linker.api.php
@@ -5,29 +5,44 @@
  */
 
 /**
- * Allows altering the label in the autocomplete.
+ * hook_relation_label_autocomplete_alter()
+ *
+ * Allows altering the label in the autocomplete field results.
  */
 function hook_relation_label_autocomplete_alter(&$label) {
-  // Do whatever with $label.
+  // Alter the text shown on the label here.
 }
 
 /**
- * Allows altering the content type name in the autocomplete.
+ * hook_relation_content_type_autocomplete_alter().
+ *
+ * Allows altering the content type name in the autocomplete field results.
  */
 function hook_relation_content_type_autocomplete_alter(&$content_type) {
-  // Do whatever with $content_type.
+  // You can alter the content type information here.
 }
 
 /**
- * Allows altering the query on the autocomplete.
+ * hook_futurium_linker_query_alter()
+ *
+ * Allows altering the query used on the autocomplete text field.
+ *
+ * @param  Object &$query [description]
+ *
  */
 function hook_futurium_linker_query_alter(&$query) {
-  // Do whatever with $query.
+  // Alter the query object.
 }
 
 /**
- * Allows overriding the relations that are possible.
+ * hook_futurium_linker_possible_relations()
+ *
+ * Allows overriding the relations that are possible from an origin node.
+ *
+ * @param  Object $origin_node         The origin node object
+ * @param  Array  &$possible_relations An array with the possible relation objects.
+ *
  */
-function hook_futurium_linker_possible_relations(&$possible_relations) {
-  // Do whatever with $possible_relations.
+function hook_futurium_linker_possible_relations(&$origin_node, &$possible_relations) {
+  // Filter the possible relations here.
 }
